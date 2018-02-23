34c9e68d24f78411bbd88d0e3dcea50dde0622eb
vagrant <vagrant@localhost.localdomain>
2018-02-23 05:08:01 +0000

    initial cookbook workstation commit

---
+++ recipes/setup.rb
@@ -0,0 +1,13 @@
+package 'tree' do
+  action :install
+end
+
+package 'git'
+
+package 'ntp'
+
+file 'etc/motd' do
+  content 'This server is the property of Sanjaya'
+  owner 'root'
+  group 'root'
+end
